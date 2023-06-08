ff64a885f20e5f403d87e9cf0143d3c7ad09d0d7
iimyzf <agnaou004@gmail.com>
2023-06-08 14:05:51 +0100

    first container almost done

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -3,21 +3,47 @@ import Apollo from "../assets/Apollo.jpg";
 import { FiLogOut } from "react-icons/fi";
 import { BsArrowRightShort } from "react-icons/bs";
 import { FiPlus } from "react-icons/fi";
+import { useState } from "react";
 
 const Chat = () => {
+    const [channels, setChannels] = useState(["Channel 1"]);
+
+    const addChannel = () => {
+        const newChannel = [...channels, `Channel ${channels.length + 1}`];
+        setChannels(newChannel);
+    };
+
     return (
         <div className="parent flex justify-center items-center">
             <div className="child-container-1 pr-3">
-                <div className="container-1 font-satoshi text-white w-[20em] h-[55em] flex flex-col justify-center items-center relative">
-                    <h3 className="absolute top-8 uppercase font-bold">
+                <div className="container-1 font-satoshi text-white w-[20em] h-[55em] flex flex-col items-center relative ">
+                    <h3 className="absolute top-7 uppercase font-bold">
                         Channels
                     </h3>
                     <span className="line absolute top-20"></span>
-                    <a href="#">
-                        <span className="plus-icon w-[4em] h-[4em] rounded-full absolute bottom-5 right-5 flex justify-center items-center">
+                    <span className="line absolute bottom-24"></span>
+                    <a href="#" onClick={addChannel}>
+                        <span className="plus-icon w-[4em] h-[4em] rounded-full absolute bottom-4 right-5 flex justify-center items-center">
                             <FiPlus className="text-3xl" />
                         </span>
                     </a>
+                    <div className="red-divs  h-[55em] w-full">
+                        {channels.map((channel, index) => (
+                            <div
+                                key={index}
+                                className="channel flex relative top-20 items-center px-5"
+                            >
+                                <img
+                                    className="apollo w-[2.5em] h-[2.5em] rounded-full"
+                                    src={Apollo}
+                                    alt="Apollo"
+                                />
+                                <h4 className="font-medium ml-3 uppercase">
+                                    {channel}
+                                </h4>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
             <div className="child-container-2 pl-3">
