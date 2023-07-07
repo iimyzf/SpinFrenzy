b1ecfcf5f00a20343225b1ac97cb912eebf59f02
iimyzf <agnaou004@gmail.com>
2023-07-07 22:59:20 +0100

    game mode

--- src/Pages/GameMode.tsx
+++ src/Pages/GameMode.tsx
@@ -3,14 +3,13 @@ import "./Gamemode.css";
 const GameMode = () => {
     return (
         <>
-            <div className="flex flex-col items-center space-y-40">
-                <h1 className="pt-40 text-9xl font-satoshi font-bold">GAME MODE</h1>
-                <div className="grid grid-cols-2 gap-20 justify-around">
-                    <div className="card w-96 h-96 hover:scale-105"></div>
-                    <div className="card w-96 h-96"></div>
-                    <div className="card w-96 h-96"></div>
+            <div className="flex flex-col justify-center items-center h-screen">
+                <h1 className="text-8xl font-satoshi font-black">GAME MODE</h1>
+                <div className="mt-5 flex gap-5">
+                    <div className="card w-60 h-60"></div>
+                    <div className="card w-60 h-60"></div>
+                    <div className="card w-60 h-60"></div>
                 </div>
-        
             </div>
         </>
     );
