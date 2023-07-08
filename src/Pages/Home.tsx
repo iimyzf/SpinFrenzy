9acb42160117b09587d7b1e54c7c4fd655cded73
iimyzf <agnaou004@gmail.com>
2023-07-08 15:53:36 +0100

    home page might be done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,13 +3,32 @@ import "../Pages/Home.css";
 const Home = () => {
     return (
         <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
-            <div className="astro relative w-fit h-fit">
-                <h1 className="font-black text-[15em]">RANDOM</h1>
-                <div className="absolute -top-10 right-0 z-0">
-                    <span className="bg-red-500 h-[12em] w-[12em] -rotate-12 rounded-xl flex justify-center items-center"></span>
+            <div className="relative w-fit h-fit">
+                <h1 className="font-black text-[15em]">PING PONG</h1>
+
+                <div className="absolute flex flex-col -top-40 right-0 -rotate-12">
+                    <p className="font-black text-start">CHAT</p>
+                    <span className="ctr-span ctr-span-chat h-[12em] w-[12em]"></span>
                 </div>
-                <div className="absolute -top-20 z-0">
-                    <span className="cntr bg-green-500 h-[15em] w-[15em] rotate-12 rounded-xl flex justify-center items-center"></span>
+
+                <div className="absolute flex flex-col -top-52 rotate-12">
+                    <p className="font-black text-start">FRIENDS</p>
+                    <span className="ctr-span ctr-span-friends h-[15em] w-[15em]"></span>
+                </div>
+
+                <div className="absolute flex flex-col top-80 -right-24 rotate-12">
+                    <span className="ctr-span ctr-span-home h-[10em] w-[10em]"></span>
+                    <p className="font-black text-start">HOME</p>
+                </div>
+
+                <div className="ctr absolute flex flex-col">
+                    <span className="ctr-span h-[16em] w-[16em] "></span>
+                    <p className="font-black text-start">PLAY</p>
+                </div>
+
+                <div className="absolute flex flex-col top-80 -left-24 -rotate-12">
+                    <span className="ctr-span ctr-span-exit h-[12em] w-[12em]"></span>
+                    <p className="font-black text-start">EXIT</p>
                 </div>
             </div>
         </div>
