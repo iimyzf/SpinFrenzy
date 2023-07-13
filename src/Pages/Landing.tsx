d1abdc493a5b9826e1705a388f458cf1bdb0bc41
iimyzf <agnaou004@gmail.com>
2023-07-13 14:22:35 +0100

    landing is responsive

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -12,22 +12,27 @@ const Landing = () => {
                     backgroundImage: "url(" + backgroundImg + ")",
                     filter: "blur(2px)",
                     backgroundSize: "cover",
+                    backgroundPosition: "center",
                     height: "100vh",
                     zIndex: -1,
                 }}
             ></div>
             <div className="text-white flex font-satoshi flex-col items-center justify-center h-screen absolute top-0 left-0 w-full">
                 <Navbar />
-                <h4 className="text-center font-normal">
+                <h4 className="text-center font-normal xs:text-[1em] xs:leading-4 xs:px-5">
                     YOU DON’T HAVE TO BE IN THE SAME ROOM TO PLAY
                 </h4>
-                <div className="container m-8 w-[35em] h-[25em] flex flex-col justify-center items-center leading-[8em]">
-                    <div className="flex flex-col justify-center items-start">
-                        <p className="font-black text-[10em]">PING</p>
-                        <p className="font-black text-[10em]">PONG</p>
+                <div className="container m-8 xs:w-[20em] xs:h-[15em] sm:w-[35em] sm:h-[25em] flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-start xs:leading-[4em] sm:leading-[8em]">
+                        <p className="font-black xs:text-[5em] sm:text-[10em]">
+                            PING
+                        </p>
+                        <p className="font-black xs:text-[5em] sm:text-[10em]">
+                            PONG
+                        </p>
                     </div>
                 </div>
-                <h3 className="login font-semibold text-[1.2em]">
+                <h3 className="login font-semibold xs:text-[1em] sm:text-[1.2em]">
                     <Link to="/profile">42 LOGIN</Link>
                 </h3>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,3 +18,10 @@ export default {
     },
     plugins: [],
 };
+
+
+//       'sm': '640px',
+//       'md': '768px',
+//       'lg': '1024px',
+//       'xl': '1280px',
+//       '2xl': '1536px',
