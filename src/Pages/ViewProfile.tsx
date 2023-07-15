0dc0813c9111a9544b8f0f66aeb6fd4c05189a30
iimyzf <agnaou004@gmail.com>
2023-07-15 17:32:17 +0100

    view-profile is responsive, I guess

--- src/Pages/ViewProfile.tsx
+++ src/Pages/ViewProfile.tsx
@@ -67,24 +67,24 @@ const ViewProfile = () => {
     }, []);
 
     return (
-        <div className="parent flex justify-center items-center">
-            <div className="child-container-1 pr-3">
-                <div className="container-1 font-satoshi text-white w-[20em] h-[55em] flex flex-col justify-center items-center relative">
-                    <div className="img-holder absolute top-[8em]">
+        <div className="parent flex xs:flex-col xl:flex-row justify-center items-center xs:gap-5 xl:gap-0 xs:m-5 xl:m-0">
+            <div className="child-container-1 xl:h-screen xl:pr-3">
+                <div className="container-1 font-satoshi text-white xs:w-[20em] xs:h-[35em] xl:w-[20em] xl:h-[55em] flex flex-col justify-center items-center relative">
+                    <div className="img-holder absolute xs:top-[3em] xl:top-[8em]">
                         <img
-                            className="w-[8em] h-[8em] rounded-full"
+                            className="xs:w-[6em] xs:h-[6em] xl:w-[8em] xl:h-[8em] rounded-full"
                             src={Apollo}
                             alt="Apollo"
                         />
-                        <span className="status rounded-full bg-green-400 w-[1.8em] h-[1.8em] absolute top-1 right-1"></span>
+                        <span className="status rounded-full bg-green-400 xs:w-[1.3em] xs:h-[1.3em] xl:w-[1.8em] xl:h-[1.8em] absolute top-1 right-1"></span>
                     </div>
-                    <h4 className="font-light absolute top-[18em]">
+                    <h4 className="font-light absolute xs:top-[10em] xl:top-[18em]">
                         @USERNAME
                     </h4>
-                    <h3 className="font-bold absolute top-[19.5em]">
+                    <h3 className="font-bold absolute xs:top-[11.5em] xl:top-[19.5em]">
                         MAMELLA INDUSTRY
                     </h3>
-                    <div className="bio flex absolute top-[22em] justify-center items-start">
+                    <div className="bio flex absolute xs:top-[14em] xl:top-[22em] justify-center items-start">
                         <p className="font-light w-[16em] leading-5 text-ellipsis text-start">
                             Lorem ipsum dolor sit amet consectetur, adipisicing
                             elit. Quas, quis quae nulla optio suscipit libero
@@ -94,7 +94,7 @@ const ViewProfile = () => {
                             consectetured
                         </p>
                     </div>
-                    <ul className="flex gap-5 absolute bottom-20">
+                    <ul className="flex gap-5 absolute xs:bottom-14 xl:bottom-20">
                         <li>
                             <a href="#">
                                 <BsGithub />
@@ -113,19 +113,19 @@ const ViewProfile = () => {
                     </ul>
                 </div>
             </div>
-            <div className="child-container-2 pl-3">
-                <div className="container-2 font-satoshi text-white w-[80em] h-[55em] flex flex-col justify-center items-center relative">
-                    <div className="editable absolute left-20 top-20">
+            <div className="child-container-2 xl:h-screen xl:pl-3">
+                <div className="container-2 font-satoshi text-white xs:w-[20em] xs:h-[35em] xl:w-[80em] xl:h-[55em] flex flex-col justify-center items-center relative">
+                    <div className="editable absolute xs:top-14 xl:left-20 xl:top-16">
                         <h3 className="pb-3 uppercase">
                             Match Statistics for Username
                         </h3>
                     </div>
                     <Chart
-                        className="chart flex justify-center w-[70em] absolute bottom-6"
+                        className="chart flex justify-center xl:w-[70em] absolute xs:top-60 xl:top-40"
                         options={chartOptions}
                         series={chartSeries}
                         type="area"
-                        // height={400}
+                        // height={300}
                         // width="1000"
                     />
                 </div>
