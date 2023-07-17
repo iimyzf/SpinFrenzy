e9388cf501c65739bbcc4b658353f65888c41c5a
iimyzf <agnaou004@gmail.com>
2023-07-17 16:14:44 +0100

    game mode is done and responsive

--- src/Pages/GameMode.tsx
+++ src/Pages/GameMode.tsx
@@ -4,11 +4,22 @@ const GameMode = () => {
     return (
         <>
             <div className="flex flex-col justify-center items-center h-screen">
-                <h1 className="text-8xl font-satoshi font-black">GAME MODE</h1>
-                <div className="mt-5 flex gap-5">
-                    <div className="card w-60 h-60"></div>
-                    <div className="card w-60 h-60"></div>
-                    <div className="card w-60 h-60"></div>
+                <h1 className="xs:text-5xl xl:text-6xl font-satoshi font-black">
+                    GAME MODE
+                </h1>
+                <p className="font-satoshi font-normal uppercase">
+                    Please select a level to play in
+                </p>
+                <div className="mt-10 xs:flex xs:flex-wrap xs:justify-center xs:items-center xs:gap-2 xl:gap-5">
+                    <div className="card xs:w-40 xs:h-40 xl:w-52 xl:h-52 flex justify-center items-center hover:cursor-pointer hover:scale-105">
+                        <p className="font-black text-2xl">EASY</p>
+                    </div>
+                    <div className="card xs:w-40 xs:h-40 xl:w-52 xl:h-52 flex justify-center items-center hover:cursor-pointer hover:scale-105">
+                        <p className="font-black text-2xl">MEDIUM</p>
+                    </div>
+                    <div className="card xs:w-40 xs:h-40 xl:w-52 xl:h-52 flex justify-center items-center hover:cursor-pointer hover:scale-105">
+                        <p className="font-black text-2xl">HARD</p>
+                    </div>
                 </div>
             </div>
         </>
