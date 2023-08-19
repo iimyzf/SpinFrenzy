858daecb22c6eac999cd1e967b331bca159b2e4d
iimyzf <agnaou004@gmail.com>
2023-08-19 12:32:12 +0100

    fixed the height and width of the divs

--- src/Pages/New.tsx
+++ src/Pages/New.tsx
@@ -10,8 +10,8 @@ import Apollo from "../assets/Apollo.jpg";
 
 const New = () => {
     return (
-        <div>
-            <div className="first-container h-20 container-1 mt-10 mx-10 px-10 flex justify-between items-center">
+        <div className="">
+            <div className="first-container h-[5vw] container-1 mt-10 mx-10 px-10 flex justify-between items-center">
                 <h2 className="font-medium lowercase font-satoshi">
                     Dashboard
                 </h2>
@@ -28,7 +28,7 @@ const New = () => {
                 </div>
             </div>
             <div className="flex">
-                <div className="friends-container container-1 mt-5 py-5 ml-10 flex flex-col w-24 justify-start items-center gap-4 overflow-y-scroll no-scrollbar h-[49.25rem]">
+                <div className="friends-container container-1 mt-5 py-5 ml-10 flex flex-col w-[6vw] max-h-[98vh] justify-start items-center gap-4 overflow-y-scroll no-scrollbar bg-orange400">
                     <button className="relative">
                         <img
                             className="w-16 h-16 rounded-full"
@@ -126,9 +126,9 @@ const New = () => {
                         <span className="rounded-full bg-green-400 w-4 h-4 absolute top-0 right-0"></span>
                     </button>
                 </div>
-                <div className="bg-red300 w-full">
-                    <div className="flex gap-5">
-                        <div className="second-container container-1 mt-5 ml-5 p-10 w-1/2 h-96">
+                <div className="w-full">
+                    <div className="flex gap-5 bg-purple600 h-[50vh]">
+                        <div className="second-container container-1 mt-5 ml-5 p-10 w-1/2 bg-blue400">
                             <h2 className="font-medium font-satoshi lowercase">
                                 Play a Game
                             </h2>
@@ -153,7 +153,7 @@ const New = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="third-container container-1 mt-5 mr-10 p-10 w-1/2 h-96 overflow-y-scroll no-scrollbar">
+                        <div className="third-container container-1 mt-5 mr-10 p-10 w-1/2 overflow-y-scroll no-scrollbar bg-green400">
                             <h2 className="font-medium font-satoshi lowercase">
                                 live games
                             </h2>
@@ -259,8 +259,8 @@ const New = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex gap-5">
-                        <div className="forth-container container-1 mt-5 ml-5 p-10 w-1/2 h-96 overflow-y-scroll no-scrollbar">
+                    <div className="flex gap-5 bg-pink600 h-[50vh]">
+                        <div className="forth-container container-1 mt-5 ml-5 p-10 w-1/2 overflow-y-scroll no-scrollbar bg-red400">
                             <h2 className="font-medium font-satoshi lowercase">
                                 popular public channels
                             </h2>
@@ -344,8 +344,28 @@ const New = () => {
                                     join
                                 </button>
                             </div>
+                            <div className="channel-div mt-5 flex justify-between container-1 h-20 px-10 items-center">
+                                <div className="flex items-center gap-5">
+                                    <img
+                                        className="rounded-full w-14 h-14"
+                                        src={Apollo}
+                                        alt="channel-pic"
+                                    />
+                                    <div className="flex flex-col">
+                                        <h2 className="font-medium font-satoshi lowercase">
+                                            name of the channel
+                                        </h2>
+                                        <h3 className="font-light font-satoshi lowercase">
+                                            42 members
+                                        </h3>
+                                    </div>
+                                </div>
+                                <button className="container-1 w-24 h-12 lowercase hover:scale-105 hover:cursor-pointer">
+                                    join
+                                </button>
+                            </div>
                         </div>
-                        <div className="forth-container container-1 mt-5 mr-10 p-10 w-1/2 h-96 overflow-y-scroll no-scrollbar">
+                        <div className="forth-container container-1 mt-5 mr-10 p-10 w-1/2 overflow-y-scroll no-scrollbar bg-yellow400">
                             <h2 className="font-medium font-satoshi lowercase">
                                 leaderboard
                             </h2>
