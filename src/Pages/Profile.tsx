de68092a2ad7f53839d23f3c36fd422299c856dd
iimyzf <agnaou004@gmail.com>
2023-08-28 16:42:00 +0100

    profile is done, but aint responsive yet

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -70,13 +70,13 @@ const Profile = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="parent flex xs:flex-col xl:flex-row justify-center items-center xs:gap-5 xl:gap-0 xs:m-5 xl:m-0">
-            <div className="child-container-1 xl:h-screen xl:pr-3">
-                <div className="container-1 font-satoshi text-white xs:w-[20em] xs:h-[35em] xl:w-[20em] xl:h-[55em] flex flex-col justify-center items-center relative">
-                    <div className="img-holder absolute xs:top-[3em] xl:top-[8em]">
+        <div className="parent flex justify-center items-center gap-[1.2vw] h-screen">
+            <div className="child-container-1">
+                <div className="container-1 font-satoshi text-white w-[18vw] h-[50vw] flex flex-col justify-center items-center relative">
+                    <div className="img-holder absolute top-[8vw]">
                         <label htmlFor="imageInput">
                             <img
-                                className="xs:w-[6em] xs:h-[6em] xl:w-[8em] xl:h-[8em] rounded-full cursor-pointer"
+                                className="w-[8vw] h-[8vw] rounded-full cursor-pointer"
                                 src={
                                     image ? URL.createObjectURL(image) : Apollo
                                 }
@@ -92,40 +92,40 @@ const Profile = () => {
                         />
                         <span
                             onClick={handleOnline}
-                            className={`status rounded-full xs:w-[1.3em] xs:h-[1.3em] xl:w-[1.8em] xl:h-[1.8em] absolute top-1 right-1 ${
+                            className={`status rounded-full w-[1.8vw] h-[1.8vw] absolute top-0 right-[.5vw] ${
                                 online ? "bg-green-400" : "bg-gray-400"
                             }`}
                         ></span>
                     </div>
-                    <h4 className="font-light absolute xs:top-[10em] xl:top-[18em] opacity-80">
+                    <h4 className="font-light absolute top-[17.5vw] opacity-80 text-[1vw]">
                         @{username}
                     </h4>
-                    <h3 className="font-bold absolute xs:top-[11.5em] xl:top-[19.5em]">
+                    <h3 className="font-bold absolute top-[18.8vw] text-[1.1vw]">
                         {fullName}
                     </h3>
-                    <div className="bio absolute xs:top-[14em] xl:top-[22em] justify-center items-start">
+                    <div className="bio absolute top-[21.5vw] justify-center items-start mx-[1vw] text-[1vw]">
                         {isBioEditing ? (
                             <>
                                 <textarea
-                                    className="flex font-light w-[16em] leading-5 text-start custom-textarea"
+                                    className="flex font-normal w-full text-start custom-textarea text-[1vw]"
                                     value={bio}
-                                    maxLength={256}
+                                    maxLength={200}
                                     onChange={handleBioChange}
                                 />
                                 <button
                                     onClick={handleBioSave}
-                                    className="float-right font-medium underline"
+                                    className="float-right mt-[1vw] font-medium underline"
                                 >
                                     save
                                 </button>
                             </>
                         ) : (
                             <>
-                                <p className="font-light w-[16em] leading-5 text-start">
+                                <p className="font-normal w-full text-start text-[1vw]">
                                     {bio}
                                 </p>
                                 <button
-                                    className="float-right mt-5 font-medium underline"
+                                    className="float-right mt-[1vw] font-medium underline"
                                     onClick={handleBioEdit}
                                 >
                                     edit bio
@@ -134,109 +134,129 @@ const Profile = () => {
                         )}
                     </div>
 
-                    <ul className="flex gap-5 absolute xs:bottom-14 xl:bottom-20">
+                    <ul className="flex gap-[2vw] absolute bottom-[5vw]">
                         <li>
                             <a href={github} target="_blank">
-                                <BsGithub />
+                                <BsGithub className="text-[1.2vw]" />
                             </a>
                         </li>
                         <li>
                             <a href={linkedin} target="_blank">
-                                <BsLinkedin />
+                                <BsLinkedin className="text-[1.2vw]" />
                             </a>
                         </li>
                         <li>
                             <a href={instagram} target="_blank">
-                                <BsInstagram />
+                                <BsInstagram className="text-[1.2vw]" />
                             </a>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div className="child-container-2 xl:h-screen xl:pl-3">
-                <div className="container-2 font-satoshi text-white xs:w-[20em] xs:h-[45em] xl:w-[80em] xl:h-[55em] flex flex-col justify-center items-center">
-                    <div className="editable">
-                        <h3 className="xs:text-[.7em] xl:text-[1.1em] pb-3">
-                            USERNAME
-                        </h3>
-                        <div className="flex gap-5">
-                            <input
-                                onChange={handleUsernameChange}
-                                type="text"
-                                maxLength={24}
-                                className="xs:w-[17em] xs:h-10 xl:w-[65em] xl:h-14 rounded-[12px] input-container outline-none indent-5"
-                            />
+            <div className="child-container-2">
+                <div className="container-2 font-satoshi text-white w-[70vw] h-[50vw] flex flex-col justify-center items-center">
+                    <div className="flex justify-center items-center gap-[1vw]">
+                        <div className="editable">
+                            <h3 className="text-[1.2vw] font-satoshi font-medium">
+                                username
+                            </h3>
+                            <div className="flex mt-[.5vw]">
+                                <input
+                                    onChange={handleUsernameChange}
+                                    type="text"
+                                    maxLength={24}
+                                    className="w-[30vw] h-[3vw] rounded-[.6vw] input-container outline-none indent-[1vw]"
+                                />
+                            </div>
+                        </div>
+                        <div className="editable">
+                            <h3 className="text-[1.2vw] font-satoshi font-medium">
+                                full name
+                            </h3>
+                            <div className="flex mt-[.5vw]">
+                                <input
+                                    onChange={handleFullNameChange}
+                                    type="text"
+                                    maxLength={24}
+                                    className="w-[30vw] h-[3vw] rounded-[.6vw] input-container outline-none indent-[1vw]"
+                                />
+                            </div>
                         </div>
                     </div>
-                    <div className="editable pt-8">
-                        <h3 className="xs:text-[.7em] xl:text-[1.1em] pb-3">
-                            FULL NAME
+                    <div className="editable flex flex-col gap[1vw] mt-[2.5vw]">
+                        <h3 className="text-[1.2vw] pb3 font-satoshi font-medium">
+                            social links
                         </h3>
-                        <div className="flex gap-5">
-                            <input
-                                onChange={handleFullNameChange}
-                                type="text"
-                                maxLength={42}
-                                className="xs:w-[17em] xs:h-10 xl:w-[65em] xl:h-14 rounded-[12px] input-container outline-none indent-5"
-                            />
-                        </div>
-                    </div>
-                    <div className="editable pt-8">
-                        <h3 className="xs:text-[.7em] xl:text-[1.1em] pb-3">
-                            EMAIL ADDRESS
-                        </h3>
-                        <div className="flex gap-5">
-                            <input
-                                type="email"
-                                maxLength={42}
-                                className="xs:w-[17em] xs:h-10 xl:w-[65em] xl:h-14 rounded-[12px] input-container outline-none indent-5"
-                            />
-                        </div>
-                    </div>
-                    <div className="editable pt-8">
-                        <h3 className="xs:text-[.7em] xl:text-[1.1em] pb-3">
-                            SOCIAL LINKS
-                        </h3>
-                        <div className="xs:flex-col xl:flex-row flex">
-                            <span className="xs:hidden check-span xl:w-[8.6em] xl:h-14 rounded-[12px] mr-5 xl:flex justify-center items-center font-medium uppercase">
-                                GitHub
+                        <div className="flex gap-[1vw] mt-[1vw]">
+                            <span className="check-span w-[8vw] h-[3vw] rounded-[.6vw] text-[1vw] flex justify-center items-center font-medium">
+                                github
                             </span>
-                            <div className="pew flex gap-5 xs:justify-center">
+                            <div className="flex">
                                 <input
                                     onChange={handleGithubChange}
                                     type="link"
-                                    className="xs:w-[17em] xs:h-10 xl:w-[55em] xl:h-14 rounded-[12px] input-container outline-none indent-5"
+                                    className="w-[52vw] h-[3vw] rounded-[.6vw] input-container outline-none indent-[1vw]"
                                 />
                             </div>
                         </div>
-                        <div className="xs:flex-col xl:flex-row flex pt-5">
-                            <span className="xs:hidden check-span xl:w-[8.6em] xl:h-14 rounded-[12px] mr-5 xl:flex justify-center items-center font-medium uppercase">
-                                LinkedIn
+                        <div className="flex gap-[1vw] mt-[1vw]">
+                            <span className="check-span w-[8vw] h-[3vw] rounded-[.6vw] text-[1vw] flex justify-center items-center font-medium">
+                                linkedin
                             </span>
-                            <div className="pew flex gap-5 xs:justify-center">
+                            <div className="flex">
                                 <input
                                     onChange={handleLinkedinChange}
                                     type="link"
-                                    className="xs:w-[17em] xs:h-10 xl:w-[55em] xl:h-14 rounded-[12px] input-container outline-none indent-5"
+                                    className="w-[52vw] h-[3vw] rounded-[.6vw] input-container outline-none indent-[1vw]"
                                 />
                             </div>
                         </div>
-                        <div className="xs:flex-col xl:flex-row flex pt-5">
-                            <span className="xs:hidden check-span xl:w-[8.6em] xl:h-14 rounded-[12px] mr-5 xl:flex justify-center items-center font-medium uppercase">
-                                Instagram
+                        <div className="flex gap-[1vw] mt-[1vw]">
+                            <span className="check-span w-[8vw] h-[3vw] rounded-[.6vw] text-[1vw] flex justify-center items-center font-medium">
+                                instagram
                             </span>
-                            <div className="pew flex gap-5 xs:justify-center">
+                            <div className="flex">
                                 <input
                                     onChange={handleInstagramChange}
                                     type="link"
-                                    className="xs:w-[17em] xs:h-10 xl:w-[55em] xl:h-14 rounded-[12px] input-container outline-none indent-5"
+                                    className="w-[52vw] h-[3vw] rounded-[.6vw] input-container outline-none indent-[1vw]"
                                 />
                             </div>
                         </div>
                     </div>
-                    <div className="pt-12 xl:w-[65em]">
-                        <div className="child flex gap-8 justify-end items-end">
-                            <h3 className="font-light">
+                    <div className="flex items-center justify-between mt-[2.5vw] w-[61vw]">
+                        <span className="text-[1.1vw] font-medium font-satoshi">
+                            enable 2auth
+                        </span>
+                        <div className="containerr">
+                            <div className="switches-container">
+                                <input
+                                    type="radio"
+                                    id="switchOn"
+                                    name="switchPlan"
+                                    value="On"
+                                    checked={true}
+                                />
+                                <input
+                                    type="radio"
+                                    id="switchOff"
+                                    name="switchPlan"
+                                    value="Off"
+                                />
+                                <label htmlFor="switchOn">ON</label>
+                                <label htmlFor="switchOff">OFF</label>
+                                <div className="switch-wrapper">
+                                    <div className="switch">
+                                        <div>ON</div>
+                                        <div>OFF</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-[3vw] w-[61vw]">
+                        <div className="child flex gap-[4vw] justify-end items-center">
+                            <h3 className="font-light text-[1vw]">
                                 <a
                                     className=" hover:cursor-pointer"
                                     onClick={() => navigate(-1)}
@@ -244,7 +264,7 @@ const Profile = () => {
                                     CANCEL
                                 </a>
                             </h3>
-                            <h3 className="font-bold text-xl">
+                            <h3 className="font-bold text-[1.3vw]">
                                 <Link to="/chat">SAVE</Link>
                             </h3>
                         </div>
