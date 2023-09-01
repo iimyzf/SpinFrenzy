cdc2a8099e9a0d091e43f9ce0626241711108e16
iimyzf <agnaou004@gmail.com>
2023-09-01 16:56:43 +0100

    sm and md resp for view-profile

--- src/Pages/ViewProfile.tsx
+++ src/Pages/ViewProfile.tsx
@@ -71,53 +71,53 @@ const ViewProfile = () => {
     }, []);
 
     return (
-        <div className="parent flex justify-center items-center gap-[1vw] max-sm:gap-[3vw] h-screen max-sm:flex-col max-md:flex-col max-md:my-[2vh]">
+        <div className="parent flex justify-center items-center h-screen gap-[1vw] max-sm:gap-[3vw] max-sm:flex-col max-md:flex-col max-md:my-[2vh]">
             <div className="child-container-1">
-                <div className="container-1 font-satoshi text-white w-[80vw] h-[45vh] flex flex-col justify-center items-center relative">
-                    <div className="flex flex-row gap-[1vw] max-sm:gap-[3vw] items-center justify-center absolute top-[5.5vw] max-sm:top-[5.5vw]">
-                        <button className="btn-1 w-[3vw] h-[3vw] max-sm:w-[5vw] max-sm:h-[5vw] rounded-full flex justify-center items-center cursor-pointer container-1">
-                            <span className="add absolute -top-[2.5vw] font-satoshi text-white font-bold text-[.6vw] max-sm:text-[1vw]">
+                <div className="container-1 font-satoshi text-white w-[18vw] h-[90vh] max-sm:w-[80vw] max-sm:h-[50vh] max-md:w-[80vw] max-md:h-[50vh] flex flex-col justify-center items-center relative">
+                    <div className="flex flex-row gap-[1vw] max-sm:gap-[3vw] max-md:gap-[3vw] items-center justify-center absolute top-[5.5vw] max-sm:top-[5.5vw] max-md:top-[5vw]">
+                        <button className="btn-1 w-[3vw] h-[3vw] max-sm:w-[5vw] max-sm:h-[5vw] max-md:w-[5vw] max-md:h-[5vw] rounded-full flex justify-center items-center cursor-pointer container-1">
+                            <span className="add absolute -top-[2.5vw] font-satoshi text-white font-bold text-[.6vw] max-sm:text-[1.2vw] max-sm:-top-[4vw] max-md:text-[1vw] max-md:-top-[4vw]">
                                 Add
                                 <br />
                                 @USERNAME
                             </span>
-                            <BsPersonFillAdd className="text-[1vw] max-sm:text-[2vw]" />
+                            <BsPersonFillAdd className="text-[1vw] max-sm:text-[2vw] max-md:text-[2vw]" />
                         </button>
-                        <button className="btn-2 w-[3vw] h-[3vw] max-sm:w-[5vw] max-sm:h-[5vw] rounded-full flex justify-center items-center cursor-pointer container-1">
-                            <span className="message absolute -top-[2.5vw] font-satoshi text-white font-bold text-[.6vw] max-sm:text-[1vw]">
+                        <button className="btn-2 w-[3vw] h-[3vw] max-sm:w-[5vw] max-sm:h-[5vw] max-md:w-[5vw] max-md:h-[5vw] rounded-full flex justify-center items-center cursor-pointer container-1">
+                            <span className="message absolute -top-[2.5vw] font-satoshi text-white font-bold text-[.6vw] max-sm:text-[1.2vw] max-sm:-top-[4vw] max-md:text-[1vw] max-md:-top-[4vw]">
                                 Message
                                 <br />
                                 @USERNAME
                             </span>
-                            <BsFillChatSquareTextFill className="text-[1vw] max-sm:text-[2vw]" />
+                            <BsFillChatSquareTextFill className="text-[1vw] max-sm:text-[2vw] max-md:text-[2vw]" />
                         </button>
-                        <button className="btn-3 w-[3vw] h-[3vw] max-sm:w-[5vw] max-sm:h-[5vw] rounded-full flex justify-center items-center cursor-pointer container-1">
-                            <span className="block absolute -top-[2.5vw] font-satoshi text-white font-bold text-[.6vw] max-sm:text-[1vw]">
+                        <button className="btn-3 w-[3vw] h-[3vw] max-sm:w-[5vw] max-sm:h-[5vw] max-md:w-[5vw] max-md:h-[5vw] rounded-full flex justify-center items-center cursor-pointer container-1">
+                            <span className="block absolute -top-[2.5vw] font-satoshi text-white font-bold text-[.6vw] max-sm:text-[1.2vw] max-sm:-top-[4vw] max-md:text-[1vw] max-md:-top-[4vw]">
                                 Block
                                 <br />
                                 @USERNAME
                             </span>
-                            <BsPersonFillSlash className="text-[1vw] max-sm:text-[2vw]" />
+                            <BsPersonFillSlash className="text-[1vw] max-sm:text-[2vw] max-md:text-[2vw]" />
                         </button>
                     </div>
-                    <div className="img-holder absolute top-[10.5vw] max-sm:top-[14vw]">
+                    <div className="img-holder absolute top-[10.5vw] max-sm:top-[14vw] max-md:top-[12vw]">
                         <div className="">
                             <img
-                                className="w-[7vw] h-[7vw] max-sm:w-[20vw] max-sm:h-[20vw] rounded-full"
+                                className="w-[7vw] h-[7vw] max-sm:w-[20vw] max-sm:h-[20vw] max-md:w-[15vw] max-md:h-[15vw] rounded-full"
                                 src={Apollo}
                                 alt="Apollo"
                             />
-                            <span className="status rounded-full bg-green-400 w-[1.5vw] h-[1.5vw] max-sm:w-[4vw] max-sm:h-[4vw] absolute top-0 right-[.5vw]"></span>
+                            <span className="status rounded-full bg-green-400 w-[1.5vw] h-[1.5vw] max-sm:w-[4vw] max-sm:h-[4vw] max-md:w-[3vw] max-md:h-[3vw] absolute top-0 right-[.5vw]"></span>
                         </div>
                     </div>
-                    <h4 className="font-light absolute top-[18vw] text-[1vw] max-sm:top-[36vw] max-sm:text-[2vw]">
+                    <h4 className="font-light absolute top-[18vw] text-[1vw] max-sm:top-[36vw] max-sm:text-[2.2vw] max-md:top-[28vw] max-md:text-[1.5vw]">
                         @mamella
                     </h4>
-                    <h3 className="font-bold absolute top-[19.5vw] text-[1vw] max-sm:top-[39vw] max-sm:text-[2vw]">
+                    <h3 className="font-bold absolute top-[19.5vw] text-[1vw] max-sm:top-[39vw] max-sm:text-[2.5vw] max-md:top-[30vw] max-md:text-[1.5vw]">
                         Mamella Industry
                     </h3>
-                    <div className="bio flex absolute top-[22vw] max-sm:top-[45vw] justify-center items-start">
-                        <p className="font-light w-[15vw] max-sm:w-full text-ellipsis text-start text-[1vw] max-sm:text-[2.5vw] max-sm:px-[2vw]">
+                    <div className="bio flex absolute top-[22vw] max-sm:top-[45vw] max-md:top-[34vw] justify-center items-start">
+                        <p className="font-light w-[15vw] max-sm:w-full max-md:w-full text-ellipsis text-start text-[1vw] max-sm:text-[2.8vw] max-sm:px-[3vw] max-md:text-[2vw] max-md:px-[3vw]">
                             Lorem ipsum dolor sit amet consectetur, adipisicing
                             elit. Quas, quis quae nulla optio suscipit libero
                             excepturi omnis cum, quidem cupiditate
@@ -126,11 +126,11 @@ const ViewProfile = () => {
                             consectetured
                         </p>
                     </div>
-                    <ul className="flex gap-[2vw] max-sm:gap-[3vw] absolute bottom-[4vw] max-sm:bottom-[10vw]">
+                    <ul className="flex gap-[2vw] max-sm:gap-[3vw] max-md:gap-[4vw] absolute bottom-[4vw] max-sm:bottom-[6vw] max-md:bottom-[6vw]">
                         <li>
                             <a
                                 href="#"
-                                className="text-[1vw] max-sm:text-[3vw]"
+                                className="text-[1vw] max-sm:text-[3vw] max-md:text-[2.4vw]"
                             >
                                 <BsGithub />
                             </a>
@@ -138,7 +138,7 @@ const ViewProfile = () => {
                         <li>
                             <a
                                 href="#"
-                                className="text-[1vw] max-sm:text-[3vw]"
+                                className="text-[1vw] max-sm:text-[3vw] max-md:text-[2.4vw]"
                             >
                                 <BsLinkedin />
                             </a>
@@ -146,7 +146,7 @@ const ViewProfile = () => {
                         <li>
                             <a
                                 href="#"
-                                className="text-[1vw] max-sm:text-[3vw]"
+                                className="text-[1vw] max-sm:text-[3vw] max-md:text-[2.4vw]"
                             >
                                 <BsInstagram />
                             </a>
@@ -155,14 +155,14 @@ const ViewProfile = () => {
                 </div>
             </div>
             <div className="child-container-2">
-                <div className="container-2 font-satoshi text-white w-[80vw] h-[50vh] flex flex-col justify-center items-center relative overflow-y-auto">
-                    <div className="editable absolute left-[2vw] top-[3vw]">
-                        <h3 className="text-[1vw]">
+                <div className="container-2 font-satoshi text-white w-[75vw] h-[90vh] max-sm:w-[80vw] max-sm:h-[50vh] max-md:w-[80vw] max-md:h-[50vh] flex flex-col justify-center items-center relative overflow-y-scroll no-scrollbar overflow-hidden">
+                    <div className="editable absolute left-[2vw] top-[3vw] max-sm:left-[5vw] max-sm:top-[5vw] max-md:left-[5vw] max-md:top-[5vw]">
+                        <h3 className="text-[1vw] max-sm:text-[2.5vw] max-md:text-[2vw]">
                             Match Statistics for <strong>Username</strong>
                         </h3>
                     </div>
                     <Chart
-                        className="w-[60vw] h-full pt-[10vw] text-[1vw]"
+                        className="w-[60vw] h-full max-sm:w-[70vw] max-sm:h-full max-md:w-[70vw] max-md:h-full pt-[10vw] max-sm:pt-[30vw] max-md:pt-[15vw] text-[1vw] max-sm:text-[1vw]"
                         options={chartOptions}
                         series={chartSeries}
                         type="area"
