966cfda8ce15e6c1eebb1864a8c94d29a303b0a0
Abderrahmane Laajili <alaajili@c4r7p6.1337.ma>
2023-09-02 12:40:04 +0100

    endMatch

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -17,6 +17,8 @@ interface GameData {
 	rightPlayerY: number;
 	speedX: number;
 	speedY: number;
+  leftScore: number;
+  rightScore: number;
 }
 
 interface PlayerData {
@@ -38,6 +40,7 @@ function Game() {
   const [playerOne, setPlayerOne] = useState<PlayerData>();
   const [playerTwo, setPlayerTwo] = useState<PlayerData>();
   const [scale, setScale] = useState<number>(1);
+  const [endMatch, setEndMatch] = useState<boolean>(false);
 
 
   const handleWindowResize = () => {
@@ -98,9 +101,14 @@ function Game() {
       setData(data);
     });
 
+    socket?.on("endMatch", () => {
+      setEndMatch(true);
+    });
+
     return () => {
       socket?.off("update");
       socket?.off("join_room");
+      socket?.disconnect();
     };
 
   }, [socket]);
@@ -113,45 +121,52 @@ function Game() {
     socket?.emit("move", { posY, roomName });
   }
 
-  if (!started) {
+
+  if (endMatch) {
+    socket?.disconnect();
+    return (
+      <div className='bg-black text-white text-xl'>
+        --- End Match ---
+      </div>
+    );
+  }
+  else if (!started) {
     return (
       <div className='bg-black text-white text-xl'>
         --- waiting another player to join you ---
       </div>
     );
   }
-  else {
-    return (
-      <div className="bg-black h-screen flex flex-col py-12  items-center space-y-4  overflow-y-scroll">
-        <div className="absolute top-4 left-4 flex items-center space-x-2">
-        <div className=" w-12 h-12 rounded-full bg-gray-400 "></div>
-        <span className="text-white text-xl font-bold">leave the match</span>
-        </div>
-        <div className="flex space-x-16 lg:space-x-48 items-center">
-          <span className="flex flex-col items-center space-y-2">
-            <img src={playerOne?.avatar} className="h-16 lg:h-20 w-16 lg:w-20 rounded-full" />
-            <span className='text-white text-lg font-mono font-bold'>{playerOne?.name}</span>
-          </span>
-          <span className="text-4xl lg:text-6xl text-white font-bold">VS</span>
-          <span className="flex flex-col items-center space-y-2">
-          <img src={playerTwo?.avatar} className="h-16 lg:h-20 w-16 lg:w-20 rounded-full" />
-            <span className='text-white text-lg font-mono font-bold'>{playerTwo?.name}</span>
-          </span>
-        </div>
-        <div  className='canvas' onMouseMove={handleMouseMove}>
-          <ReactP5Wrapper sketch={GameField} leftPlayerY={data?.leftPlayerY} rightPlayerY={data?.rightPlayerY} ball={data?.ballPos}/>
-        </div>
-        <div className="flex flex-col items-center">
-        <span className="text-2xl lg:text-4xl text-white font-bold underline">score</span>
-        <div className="flex space-x-16 lg:space-x-48 items-center">
-          <span className="text-white text-2xl lg:text-4xl font-black">0</span>
+  return (
+    <div className="bg-black h-screen flex flex-col py-12  items-center space-y-4  overflow-y-scroll">
+    <div className="absolute top-4 left-4 flex items-center space-x-2">
+    <div className=" w-12 h-12 rounded-full bg-gray-400 "></div>
+    <span className="text-white text-xl font-bold">leave the match</span>
+    </div>
+    <div className="flex space-x-16 lg:space-x-48 items-center">
+      <span className="flex flex-col items-center space-y-2">
+        <img src={playerOne?.avatar} className="h-16 lg:h-20 w-16 lg:w-20 rounded-full" />
+        <span className='text-white text-lg font-mono font-bold'>{playerOne?.name}</span>
+      </span>
+      <span className="text-4xl lg:text-6xl text-white font-bold">VS</span>
+      <span className="flex flex-col items-center space-y-2">
+      <img src={playerTwo?.avatar} className="h-16 lg:h-20 w-16 lg:w-20 rounded-full" />
+        <span className='text-white text-lg font-mono font-bold'>{playerTwo?.name}</span>
+      </span>
+    </div>
+    <div  className='canvas' onMouseMove={handleMouseMove}>
+      <ReactP5Wrapper sketch={GameField} leftPlayerY={data?.leftPlayerY} rightPlayerY={data?.rightPlayerY} ball={data?.ballPos}/>
+    </div>
+    <div className="flex flex-col items-center">
+    <span className="text-2xl lg:text-4xl text-white font-bold underline">score</span>
+    <div className="flex space-x-16 lg:space-x-48 items-center">
+          <span className="text-white text-2xl lg:text-4xl font-black">{data?.leftScore}</span>
           <span className="text-white text-2xl lg:text-4xl font-black">:</span>
-          <span className="text-white text-2xl lg:text-4xl font-black">0</span>
+          <span className="text-white text-2xl lg:text-4xl font-black">{data?.rightScore}</span>
         </div>
       </div>
     </div>
   );
-  }
 }
 
 export default Game;
